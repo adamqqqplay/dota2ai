a92e13b0edaf5b7f9ecdecdae33e22ae3a1fb71c
spc581 <43434690+spc581@users.noreply.github.com>
2019-12-13 04:41:01 -0500

    Add files via upload

--- item_purchase_warlock.lua
+++ item_purchase_warlock.lua
@@ -1,48 +1,41 @@
 ----------------------------------------------------------------------------
---	Ranked Matchmaking AI v1.0a
+--	Ranked Matchmaking AI v1.6b
 --	Author: adamqqq		Email:adamqqq@163.com
 ----------------------------------------------------------------------------
-local utility = require( GetScriptDirectory().."/utility" ) 
+local ItemPurchaseSystem = dofile(GetScriptDirectory() .. "/util/ItemPurchaseSystem")
 
 local ItemsToBuy = 
 { 
 	"item_tango",
+	"item_tango",
 	"item_clarity",
-	"item_branches",
-	"item_branches",
-	
-	"item_boots",	
+	"item_flask",
+	"item_ring_of_basilius",
+
+
+	"item_arcane_boots",
 	"item_magic_stick",
-	"item_recipe_magic_wand",		--大魔棒7.14
-	"item_energy_booster",			--秘法鞋
+
+	"item_hand_of_midas",
+
+
 	
-	"item_cloak",
-	"item_shadow_amulet",			--微光
 	
-	"item_ring_of_regen",
-	"item_staff_of_wizardry",
-	"item_recipe_force_staff",		--推推7.14
+	"item_glimmer_cape",			--微光
+	"item_force_staff",
+	"item_ultimate_scepter_1",		--蓝杖
+
+	"item_refresher",
+	"item_lotus_orb",			--清莲宝珠
 	
-	"item_point_booster",
-	"item_staff_of_wizardry",
-	"item_ogre_axe",
-	"item_blade_of_alacrity",		--蓝杖
-	
-	"item_ring_of_health",
-	"item_void_stone",		
-	"item_ring_of_health",
-	"item_void_stone",		
-	"item_recipe_refresher", 		--刷新球
-	
-	"item_mystic_staff",
-	"item_ultimate_orb",
-	"item_void_stone",				--羊刀
+	"item_sheepstick",				--羊刀
 }
 
-utility.checkItemBuild(ItemsToBuy)
+local Transfered = ItemPurchaseSystem.Transfer(ItemsToBuy)
+ItemPurchaseSystem.checkItemBuild(Transfered)
 
 function ItemPurchaseThink()
-	utility.BuySupportItem()
-	utility.BuyCourier()
-	utility.ItemPurchase(ItemsToBuy)
+	ItemPurchaseSystem.BuySupportItem()
+	ItemPurchaseSystem.BuyCourier()
+	ItemPurchaseSystem.ItemPurchase(Transfered)
 end
--- item_purchase_weaver.lua
+++ item_purchase_weaver.lua
@@ -1,53 +1,39 @@
 ----------------------------------------------------------------------------
---	Ranked Matchmaking AI v1.5c
+--	Ranked Matchmaking AI v1.6b
 --	Author: adamqqq		Email:adamqqq@163.com
 --  Contributor: zmcmcc Email:mengzhang@utexas.edu
 ----------------------------------------------------------------------------
-local utility = require( GetScriptDirectory().."/utility" ) 
+local ItemPurchaseSystem = dofile(GetScriptDirectory() .. "/util/ItemPurchaseSystem")
 
 local ItemsToBuy = 
 { 
-	"item_slippers",
-	"item_circlet",
-	"item_recipe_wraith_band", --系带
+	"item_wraith_band", --系带
 	"item_tango",
+	"item_tango",
+	"item_flask",
+	"item_wraith_band", --系带
 
-	"item_branches",
-	"item_branches",
-	"item_magic_stick",
-	"item_recipe_magic_wand",		--大魔棒7.14
+	"item_magic_wand",		--大魔棒7.14
 
-	"item_ring_of_protection",
-	"item_sobi_mask",				--天鹰
+	"item_power_treads",			--假腿7.21
 
-	"item_boots",
-	"item_belt_of_strength",
-	"item_gloves",					--假腿
+	"item_maelstrom",
 
-	"item_ring_of_health",
-	"item_void_stone",
-	"item_ultimate_orb",
-	"item_recipe_sphere",			--林肯
+	"item_diffusal_blade",
+
+	"item_sphere",			--林肯
 	
-	"item_blight_stone",
-    "item_mithril_hammer",
-    "item_mithril_hammer",			--黯灭
-
-	"item_ogre_axe", 
-	"item_mithril_hammer",
-	"item_recipe_black_king_bar",	--bkb
+	"item_black_king_bar",	--bkb
 	
-	"item_quarterstaff",
-	"item_eagle",
-	"item_talisman_of_evasion",		--蝴蝶
+	"item_bloodthorn",		--血棘
 	
-	"item_point_booster",
-    "item_ultimate_orb",
-    "item_ultimate_orb",			--冰眼
+	"item_hyperstone",
+	"item_recipe_mjollnir",			--大雷锤
 }
 
-utility.checkItemBuild(ItemsToBuy)
+local Transfered = ItemPurchaseSystem.Transfer(ItemsToBuy)
+ItemPurchaseSystem.checkItemBuild(Transfered)
 
 function ItemPurchaseThink()
-	utility.ItemPurchase(ItemsToBuy)
+	ItemPurchaseSystem.ItemPurchase(Transfered)
 end
