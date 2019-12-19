ae32994363aa5c91675a11a4e4b8ab42f982df71
adamqqq <adamqqq@163.com>
2019-12-19 13:01:02 +0800

    v1.6f update. Supports Dota2 version 7.23.
    1. Update the item building of 7.23.
    2. Update warding location of 7.23.
    3. Fix courier related issues.
    4. Update the title of the professional teams in TI9.
    5. Refactor item building system to automatically adapt to recipe changes caused by game version.

--- ability_item_usage_generic.lua
+++ ability_item_usage_generic.lua
@@ -8,6 +8,7 @@ module("ability_item_usage_generic", package.seeall)
 local utility = require(GetScriptDirectory() .. "/utility")
 local Courier = dofile(GetScriptDirectory() .. "/util/CourierSystem")
 local ItemUsageSystem = dofile(GetScriptDirectory() .. "/util/ItemUsageSystem")
+local ChatSystem = dofile(GetScriptDirectory() .. "/util/ChatSystem")
 
 local function ConsiderGlyph()
 	local Towers = {
@@ -57,45 +58,20 @@ local function RecordStuckState()
 	end
 end
 
-local announceFlag=false
-local function SendAnnouncement()
-	if announceFlag == false then
-		announceFlag = true;
-		for id=1,36,1
-		do
-			if(IsPlayerBot(id)==true)
-			then
-				if(GetTeamForPlayer(id)==GetTeam())
-				then
-					local npcBot=GetBot();
-					if(npcBot:GetPlayerID()==id)
-					then
-						npcBot:ActionImmediate_Chat(
-							"Welcome to Ranked Matchmaking AI, the current version is 1.6d, updated on April 15, 2019. If you have any questions or feedback, please leave message on steam workshop https://steamcommunity.com/sharedfiles/filedetails/?id=855965029 or contact adamqqq@163.com",
-							true
-						)
-						npcBot:ActionImmediate_Chat(
-							"Please use hard or unfair mode and do not play Monkey king. 请使用困难或疯狂难度，不要使用齐天大圣。",
-							true
-						)
-					end
-				end
-				return;
-			end
-		end
-	end
-end
-
-function CourierUsageThink()
+local function SecondaryOperation()
 	ConsiderGlyph()
 	ItemUsageSystem.UnImplementedItemUsage()
-	Courier.CourierUsageThink()
 	RecordStuckState()
 
 	if(DotaTime()>=-60 and DotaTime()<=-59)
 	then
-		SendAnnouncement()
+		ChatSystem.SendVersionAnnouncement()
 	end
+end
+
+function CourierUsageThink()
+	Courier.CourierUsageThink()
+	SecondaryOperation()
 end
 
 function AbilityLevelUpThink2(AbilityToLevelUp, TalentTree)
---
+++ util/ChatSystem.lua
@@ -0,0 +1,29 @@
+local BotsInit = require("game/botsinit")
+local M = BotsInit.CreateGeneric()
+
+local announceFlag = false
+function M.SendVersionAnnouncement()
+	if announceFlag == false then
+		announceFlag = true
+		for id = 1, 36, 1 do
+			if (IsPlayerBot(id) == true) then
+				if (GetTeamForPlayer(id) == GetTeam()) then
+					local npcBot = GetBot()
+					if (npcBot:GetPlayerID() == id) then
+						npcBot:ActionImmediate_Chat(
+							"Welcome to Ranked Matchmaking AI, the current version is 1.6f, updated on December 19, 2019. If you have any questions or feedback, please leave message on steam workshop https://steamcommunity.com/sharedfiles/filedetails/?id=855965029 or contact Dota2RMMAI@outlook.com",
+							true
+						)
+						npcBot:ActionImmediate_Chat(
+							"Please use hard or unfair mode and do not play Monkey king. 请使用困难或疯狂难度，不要使用齐天大圣。",
+							true
+						)
+					end
+				end
+				return
+			end
+		end
+	end
+end
+
+return M
