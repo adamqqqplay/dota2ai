17f8f44ece76dc1c8b7084fa5016a1c7fa1a1e3a
Ben Susman <bensusman@gmail.com>
2021-05-15 00:31:50 -0500

    Temporarily removing armlet while more robust implementation for usage logic can be worked through.

--- item_purchase_life_stealer.lua
+++ item_purchase_life_stealer.lua
@@ -14,7 +14,7 @@ local ItemsToBuy =
 	"item_phase_boots", --相位7.21
 	"item_orb_of_corrosion",
     "item_magic_wand", --大魔棒7.14
-	"item_armlet", --臂章
+	--"item_armlet", --臂章
     --"item_scepter_shard",
 	"item_sange_and_yasha",
 	"item_desolator",
