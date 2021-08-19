6d28c26b81919d0bcef1ba0140da6fe05c0a436a
Kyrios0 <kyr1os@qq.com>
2021-08-19 19:00:27 +0800

    Update item_purchase_tusk.lua
    Add missing ","

--- item_purchase_tusk.lua
+++ item_purchase_tusk.lua
@@ -17,7 +17,7 @@ local ItemsToBuy =
 	"item_aghanims_shard",
 	"item_black_king_bar", --bkb
 	"item_ultimate_scepter", --蓝杖
-	"item_silver_edge" --大隐刀
+	"item_silver_edge", --大隐刀
 	"item_recipe_ultimate_scepter",
 	"item_sheepstick",
 }
